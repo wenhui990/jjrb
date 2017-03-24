027405719e88f06a391b88b0d97f557b38294263
wenhui990 <729250771@qq.com>
2017-03-24 18:01:22 +0800

    搜索+表格+图形切换+统计接口

--- js/index.js
+++ js/index.js
@@ -1,11 +1,5 @@
 $(function() {
 	$(".active a").css("color","#3b5998;");
-	if (window.location.href.indexOf("index")) {
-		setTimeout(function(){
-			console.log($("#navs li").length)
-			$("#navs li").eq(0).addClass("active");
-		},500);
-	}
 	$(".nav>li").mouseover();
 	$(document).on("load",".nav>li:eq[0]",function(){
 		$(this).addClass("active")
@@ -229,7 +223,7 @@ $(function() {
     if (system.win || system.mac || system.xll ||system.ipad) {
     	console.log("电脑");
     }else{
-//  	alert("手机");
+    	console.log("手持设备");
     	$(document).on("load","#logo_img",function(){
     		$(this).css({"width":"35px","height":"35px","margin-top":"-8px"});
     	});
@@ -369,121 +363,305 @@ Array.prototype.unique3 = function(){
 	 }
 	 return res;
 }
-//图表数据获取
-function urlLoad(id,url,country,indicator,start,end,_val,echartType){
-	console.log(url);
-	if (country) {
-		url += "&country="+country;
-	}
-	if (indicator) {
-		url += "&indicator="+indicator;
-	}
-	if (start) {
-		url += "&start="+start;
-	}
-	if (end) {
-		url += "&end="+end;
-	}
-	console.log(url);
-	// 基于准备好的dom，初始化echarts图表
-	var myChart = echarts.init(document.getElementById(id));
-	if (location.href.indexOf("add_viewpoint")) {
-		$("#"+id).append("<span class='glyphicon glyphicon-remove new_close none'></span>");
-	}
-	echartType='line'
-	console.log(echartType);
-	echartType=='line'?echartType=false:echartType=true;
-	console.log(echartType);
-	myChart.showLoading();
-	//异步加载方法
-	$.get(url).done(function(data) {
-		myChart.hideLoading();
-		var d;
-//		console.log(data);
-		if(typeof(data) == "object" && 
-            Object.prototype.toString.call(data).toLowerCase() == "[object object]" && !data.length){
-            d=data;
-        }else{
-        	d = JSON.parse(data);
-			console.log(d);
-        }
+
+
+var dataDesc = {
+	//图表数据获取
+	urlLoad: function(id,url,country,indicator,start,end,echartType){
+		if (country) {
+			url += "&country="+country;
+		}
+		if (indicator) {
+			url += "&indicator="+indicator;
+		}
+		if (start) {
+			url += "&start="+start;
+		}
+		if (end) {
+			url += "&end="+end;
+		}
+		console.log(url);
+		// 基于准备好的dom，初始化echarts图表
+		var myChart = echarts.init(document.getElementById(id));
+		if (location.href.indexOf("add_viewpoint")) {
+			$("#"+id).append("<span class='glyphicon glyphicon-remove new_close none'></span>");
+		}
 		
-		var time = [],_series=[],country=[],_name="";
-//		if (!_val) {
-//			_val=[];
-//		}else{
-//			_val=_val.unique3
-//			console.log(_val)
-//		}
-		$.each(d,function(key,val){
-			var value = [];
-			_name += key+"  ";					
-			country.push(key);
-    		$.each(val,function(i,e){
-    			time.push(e.date);
-    			value.push(e.value);
-    		});
-//  		console.log(key);
-//  		console.log(val);
-    		var s = {
-    			name: key,//'邮件营销',
-				type: 'line',//'line'['line', 'bar', 'stack', 'tiled']
-//				stack: '总量',
-//				barWidth: '10%',
-				areaStyle: {
-					normal: {}
-				},
-				data: value
-			}
-    		
-    		_series.push(s);
-    		
-		});
+		var fla = true,s={};
 		
-		myChart.setOption({
-			title: {
-				text: country+" "+ indicator
-			},
-			tooltip: {
-//				trigger: 'axis'
-			},
-			legend: {
-				data: country//["邮件营销","联盟广告","视频广告","直接访问","搜索引擎"]
-			},
-			toolbox: {
-				feature: {
-					saveAsImage: {}
+		myChart.showLoading();
+		//异步加载方法
+		$.get(url).done(function(data) {
+			myChart.hideLoading();
+			var d;
+	//		console.log(data);
+			if(typeof(data) == "object" && 
+	            Object.prototype.toString.call(data).toLowerCase() == "[object object]" && !data.length){
+	            d=data;
+	        }else{
+	        	d = JSON.parse(data);
+				console.log(d);
+	        }
+			
+			var time = [],_series=[],country=[],_name="";
+	//		if (!_val) {
+	//			_val=[];
+	//		}else{
+	//			_val=_val.unique3
+	//			console.log(_val)
+	//		}
+			$.each(d,function(key,val){
+				var value = [];
+				_name += key+"  ";					
+				country.push(key);
+	    		$.each(val,function(i,e){
+	    			time.push(e.date);
+	    			value.push(e.value);
+	    		});
+	//  		console.log(key);
+	//  		console.log(val);
+				//切换图表样式
+	    		if(!echartType || echartType=='' || echartType==undefined){
+	    			echartType = 'line';
+					fla = false;
+	    		}
+	    		if (echartType==='line' && echartType) {
+					fla=false;
+					s = {
+						name: key,
+			            type: echartType,
+			            smooth:true,
+			            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+			            data: value
+					}
+				} else if(echartType==='bar' && echartType){
+					fla=true;
+					s = {
+						name: key,
+			            type: echartType,
+			            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+			            data: value
+					}
+				} else if(echartType==='oneLine' && echartType){
+					s = {
+						name: key,
+			            type: 'line',
+			            itemStyle: {normal: {areaStyle: {type: 'default'}}},
+			            data: value,
+			            markPoint : {
+			                data : [
+			                    {type : 'max', name: '最大值'},
+			                    {type : 'min', name: '最小值'}
+			                ]
+			            },
+			            markLine : {
+			                data : [
+			                    {type : 'average', name: '平均值'}
+			                ]
+			            }
+					}
+						
 				}
-			},
-//			grid: {
-//				left: '3%',
-//				right: '4%',
-//				bottom: '3%',
-//				containLabel: true
-//			},
-			xAxis: [{
-				type: 'category',
-				boundaryGap: echartType,
-				data: time.unique3()//["周一","周二","周三","周四","周五","周六","周日1312"]
-			}],
-			yAxis: [{
-				type: 'value',
-				axisLabel: {
-                    formatter: function (value, index) {
-                        var v = 0;
-//                      console.log(value);
-                        if (value >= 100000000) {
-                            v = value / 100000000;
-                            v += '亿';
-                        }else{
-                        	v = value;
-                        }
-                        return v;
-                    }
-                }
-			}],
-			series: _series
+	    		_series.push(s);
+			});
+			
+			myChart.setOption({
+				title: {
+					text: country+" "+ indicator
+				},
+				tooltip: {
+	//				trigger: 'axis'
+				},
+				legend: {
+					data: country//["邮件营销","联盟广告","视频广告","直接访问","搜索引擎"]
+				},
+				toolbox: {
+					feature: {
+						saveAsImage: {}
+					}
+				},
+	//			grid: {
+	//				left: '3%',
+	//				right: '4%',
+	//				bottom: '3%',
+	//				containLabel: true
+	//			},
+				xAxis: [{
+					type: 'category',
+					boundaryGap: fla,
+					data: time.unique3()//["周一","周二","周三","周四","周五","周六","周日1312"]
+				}],
+				yAxis: [{
+					type: 'value',
+					axisLabel: {
+	                    formatter: function (value, index) {
+	                        var v = 0;
+	//                      console.log(value);
+	                        if (value >= 100000000) {
+	                            v = value / 100000000;
+	                            v += '亿';
+	                        }else{
+	                        	v = value;
+	                        }
+	                        return v;
+	                    }
+	                }
+				}],
+				series: _series
+			});
 		});
-	});
-	
+		
+	},
+	//数据导航列
+	navList: function(filter_txt){
+		//页面加载缓冲的过滤
+		if(localStorage.filterTxt){
+			var filter_html='',
+				len=0,minlen,
+				localStorage_filterTxt = localStorage.filterTxt;
+			if(localStorage_filterTxt.indexOf(',')){
+				var lf = localStorage_filterTxt.split(',');
+				len = lf.length;
+				minlen = len<10?0:len-10;
+				for (var i=len-1;i>minlen-1;i--) {
+					filter_html += '<span><a href="javascript:void(0);" class="data_nav_filter_txt">'+lf[i]+'</a></span>';
+					filter_txt.push(lf[i]);
+					
+				}
+			}else{
+				filter_html += '<span><a href="javascript:void(0);" class="data_nav_filter_txt">'+localStorage_filterTxt+'</a></span>';
+				filter_txt.push(localStorage_filterTxt);
+				console.log(filter_html);
+			}
+			$(".nav_txt").append(filter_html);
+		}
+		
+		$.ajax({
+			type:"get",
+			url: 'http://api.jjrb.grsx.cc/data/group?type=1&with_indicator=1',
+			data:{
+				token:'w1N3dahtnIny9Vaty4WZskJiOcsICdazhzMrvdWadpNGbwu9FdaioTYny1WZt0gTOs3QWMc2czNa1QzdrhlTOdsIiZwi4WNay9Wbn9BAdt=oDM'
+			},
+			async:true,
+			success: function(data){
+				var html = '';
+				console.time("列循环");
+				/*$.each(data, function(i,e) {
+					console.log(e);
+					if (e.name!=='null' && e.name!=='' && e.name) {
+						html = '<dl class=""><dt>'+data.name+'</dt>';
+					}else{
+						return false;
+					}
+					
+					$.each(e.indicators,function(i,ev){
+						console.log(ev);
+						if (ev.name!=='null' && ev.name!=='' && ev.name) {
+							html += '<dd><a href="#" data-id="'+ev.id+'" class="data_nav_click">'+ev.name_cn+'</a></dd>';
+						}else{
+							return false;
+						}
+						
+					});
+					html += '</dl>'
+					console.log(html);
+				});*/
+				
+				//原生态for循环
+				var html = '';
+				for (var i=0;i<10;i++) {
+					var a= data[i];
+					html += '<dl class=""><dt>'+data[i].name+'</dt>';
+					for (var j=0;j<8;j++) {
+//									console.log(a.indicators[j]);
+						html += '<dd><a href="javascriptcn:void(0);" data-id="'+a.indicators[j].id+'" class="data_nav_click" data-abc = "123">'+a.indicators[j].name_cn+'</a></dd>';
+					}
+					html +='</dl>';
+				}
+				$("#data_nav_lists").append(html);
+				
+				//瀑布流引用
+				var $waterfall = $('#data_nav_lists');
+				pbl_width = $("#data_nav_lists").width();
+				$waterfall.masonry({
+					columnWidth: pbl_width/3
+				});
+				$("#inputSearch").removeProp("readonly");
+				console.timeEnd("列循环");
+				//点击最近过滤文字
+				$(document).on("click",".data_nav_filter_txt",function(){
+					var val = $(this).text();
+					$("#inputSearch").val(val).keyup();
+				});
+			}
+		});
+	},
+	//表格加载数据
+	loadDatas: function(_href,indicator){
+		console.log(_href)
+		$.ajax({
+			type:"get",
+			url: _href + "/data/stat?indicator=" + indicator,
+			async:true,
+			success: function(data){
+//							console.log(data);
+				console.time("表格数据：");
+				$.each(data,function(i,e){
+//								console.log(e);
+					var table_icon_style='',html = '<tr>';
+					if((e.previous-e.val)>0){
+						table_icon_style = 'glyphicon-arrow-up green';
+					}else{
+						table_icon_style = 'glyphicon-arrow-down red';
+					}
+					html += '<td>'+e.country+'</td>'+
+			  				'<td>'+e.val+'</td>'+
+			  				'<td><span class="table_data">'+timeF(e.created,'mm')+'</span>&nbsp;&nbsp;&nbsp;<span class="glyphicon '+table_icon_style+'"></span></td>'+
+			  				'<td>'+e.previous+'</td>'+
+			  				'<td>'+e.max+'</td>'+
+			  				'<td>'+e.min+'</td>';
+			  		html += '</tr>';
+			  		$("#data_table").find("tbody").append(html);
+				});
+				console.timeEnd("表格数据：");
+			}
+		});
+	},
+	//搜索框事件
+	dataSearch: function(filter_txt){
+		$(document).on("keyup","#inputSearch",function(e){
+			$dd = $("#data_nav_lists dd");
+			var val = $(this).val();
+			console.log(val);
+			var dTxt;
+			$dd.not(":contains('"+val+"')").hide();
+			$dd.filter(":contains('"+val+"')").each(function(i,e){
+				$(e).show();
+			});
+			if (val.length==0) {
+				$dd.show().css("color","#3b5998");
+				$dd.find("a").css("color","#3b5998");
+			}
+			var $waterfall = $('#data_nav_lists');
+			pbl_width = $("#data_nav_lists").width();
+			$waterfall.masonry({
+				columnWidth: pbl_width/3
+			});
+			
+		});
+		
+		//搜索框失去焦点加到过滤
+		$(document).on("blur","#inputSearch",function(e){
+			var val = $(this).val().trim();
+			if (val !== '' && val.length > 0) {
+				var html = '<span><a href="javascript:void(0);" class="data_nav_filter_txt">'+val+'</a></span>';
+				$(".nav_txt").append(html);
+				filter_txt.push(val);
+				filter_txt.unique3();
+				console.log(filter_txt);
+				localStorage.setItem("filterTxt",filter_txt);
+			}
+		});
+	}
 }
+
